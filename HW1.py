627b852a49b3a23264eefa596cebd114d68f1ca0
globlo <49570577+globlo@users.noreply.github.com>
2021-02-15 00:43:20 -0600

    complete by problem3

--- HW1.py
+++ HW1.py
@@ -17,11 +17,11 @@ import sys
 from Crypto.Cipher import AES
 from Crypto import Random
 from Crypto.Util import Counter
+from itertools import permutations
 
 # BEGIN SOLUTION
 # please import only standard modules and make sure that your code compiles and runs without unhandled exceptions 
 from Crypto.Cipher import AES
-#iv = '0000000000000000'.encode('utf-8')
 
 # END SOLUTION
 
@@ -32,20 +32,19 @@ def problem_1():
     #print(cipher_text)
     # BEGIN SOLUTION
     iv=bytearray(16)
-    print(iv)
     #key = bytes([0] * 16)
     rList = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
     key = bytearray(rList)
-    print(key)
+    #print(key)
         
     aes = AES.new(key, AES.MODE_CBC, iv)
     decd = aes.decrypt(cipher_text)
-    print(decd)
+    #print(decd)
 
     plain_text = decd
     # END SOLUTION
 
-    with open("plain1.txt", "wb") as plain_file:
+    with open("plain1.txt", "wb") as plain_file:  # wb means writing in binary mode
         plain_file.write(plain_text)
 
 
@@ -53,10 +52,48 @@ def problem_1():
 def problem_2():
     with open("cipher2.bin", "rb") as cipher_file:
         cipher_text = cipher_file.read()
-
+    
     # BEGIN SOLUTION
-    modified_cipher_text = cipher_text[:]
-    plain_text = modified_cipher_text
+    iv=bytearray(16)
+    rList = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+    key = bytearray(rList)
+
+    ctext1 = cipher_text[:16]
+    ctext2 = cipher_text[16:32]
+    ctext3 = cipher_text[32:]
+    # modified_cipher_text = cipher_text[:]
+
+    # count=0
+    # for c in cipher_text:
+    #     count = count +1
+    #     print("c is ",c,"| count is ",count)
+
+
+    # perm = permutations([ctext1,ctext2,ctext3])
+    # for i in list(perm):
+    #     print(i[0])
+    #     mtxt = i[0]+i[1]+i[2]
+    #     print(mtxt)
+    #     aes = AES.new(key, AES.MODE_CBC, iv)
+    #     decd = aes.decrypt(mtxt)
+    #     print("decd is ",decd)
+
+    # mtxt = ctext1+ctext2+ctext3
+    # mtxt = ctext1+ctext3+ctext2
+    # mtxt = ctext2+ctext1+ctext3
+    # mtxt = ctext2+ctext3+ctext1
+    # mtxt = ctext3+ctext1+ctext2
+    modified_cipher_text = ctext3+ctext2+ctext1
+    count=0
+    # for c in mtxt:
+    #     count = count +1
+    #     print("c is ",c,"| count is ",count)
+    aes = AES.new(key, AES.MODE_CBC, iv)
+    decd = aes.decrypt(modified_cipher_text)
+    print("decd is ",decd)
+ 
+    plain_text = decd
+    # plain_text = modified_cipher_text
     # END SOLUTION
 
     with open("plain2.txt", "wb") as plain_file:
@@ -70,7 +107,11 @@ def problem_3():
         other_bmp = message_file.read()
 
     # BEGIN SOLUTION
-    modified_cipher_bmp = cipher_bmp
+
+    header = other_bmp[:1000]  
+    modified_cipher_bmp = header + cipher_bmp[1000:]
+
+    # modified_cipher_bmp = cipher_bmp
     # END SOLUTION
 
     with open("cipher3_modified.bmp", "wb") as modified_cipher_file:
@@ -108,11 +149,11 @@ def problem_5():
 
 def main():
     try:
-        problem_1()
-        problem_2()
-        problem_3()
+        # problem_1()
+        # problem_2()
+        # problem_3()
         problem_4()
-        problem_5()
+        # problem_5()
     except Exception:
         print("Exception:")
         traceback.print_exc(file=sys.stdout)
