6211f82a74276081c9460c0aa3c23447ef3c6424
globlo <49570577+globlo@users.noreply.github.com>
2021-02-28 18:07:16 -0600

    Finished prob5

--- HW1.py
+++ HW1.py
@@ -33,14 +33,10 @@ def problem_1():
     # BEGIN SOLUTION
     iv=bytearray(16)
     key = bytearray([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16])
-    print(iv)
-    print(key)
         
     aes = AES.new(key, AES.MODE_CBC, iv)
-    decd = aes.decrypt(cipher_text)
-    #print(decd)
+    plain_text = aes.decrypt(cipher_text)
 
-    plain_text = decd
     # END SOLUTION
 
     with open("plain1.txt", "wb") as plain_file:  # wb means writing in binary mode
@@ -60,22 +56,7 @@ def problem_2():
     ctext1 = cipher_text[:16]
     ctext2 = cipher_text[16:32]
     ctext3 = cipher_text[32:]
-    # modified_cipher_text = cipher_text[:]
-
-    # count=0
-    # for c in cipher_text:
-    #     count = count +1
-    #     print("c is ",c,"| count is ",count)
-
-
-    # perm = permutations([ctext1,ctext2,ctext3])
-    # for i in list(perm):
-    #     print(i[0])
-    #     mtxt = i[0]+i[1]+i[2]
-    #     print(mtxt)
-    #     aes = AES.new(key, AES.MODE_CBC, iv)
-    #     decd = aes.decrypt(mtxt)
-    #     print("decd is ",decd)
+    
 
     # mtxt = ctext1+ctext2+ctext3
     # mtxt = ctext1+ctext3+ctext2
@@ -83,16 +64,13 @@ def problem_2():
     # mtxt = ctext2+ctext3+ctext1
     # mtxt = ctext3+ctext1+ctext2
     modified_cipher_text = ctext3+ctext2+ctext1
-    count=0
-    # for c in mtxt:
-    #     count = count +1
-    #     print("c is ",c,"| count is ",count)
+    
+
     aes = AES.new(key, AES.MODE_CBC, iv)
     decd = aes.decrypt(modified_cipher_text)
     print("decd is ",decd)
  
     plain_text = decd
-    # plain_text = modified_cipher_text
     # END SOLUTION
 
     with open("plain2.txt", "wb") as plain_file:
@@ -110,7 +88,6 @@ def problem_3():
     header = other_bmp[:1000]  
     modified_cipher_bmp = header + cipher_bmp[1000:]
 
-    # modified_cipher_bmp = cipher_bmp
     # END SOLUTION
 
     with open("cipher3_modified.bmp", "wb") as modified_cipher_file:
@@ -130,7 +107,6 @@ def problem_4():
     plain_text_b = bytes([ a ^ b ^ c for (a,b,c) in zip(bytes(plain_text_a), bytes(cipher_text_a), bytes(cipher_text_b)) ])
     print(plain_text_b)
 
-
     # END SOLUTION
 
     with open("plain4B.txt", "wb") as plain_file:
@@ -142,8 +118,24 @@ def problem_5():
         cipher_text = cipher_file.read()
 
     # BEGIN SOLUTION
-    plain_text = cipher_text
-    key = bytes([0] * 16)
+    iv=bytearray(16)
+    key = bytearray([0] * 16)
+    
+    for month in range(1,12):
+        for day in range(1,31):
+            for year in range(0,99):
+                key = bytearray([month,day,year,0,0,0,0,0,0,0,0,0,0,0,0,0])
+                aes = AES.new(key, AES.MODE_CBC, iv)
+                decd = aes.decrypt(cipher_text)
+                count =0
+                for c in decd:
+                    if 0 <= c and c <= 128:
+                        count = count +1
+                        if count == len(decd):
+                            print("Moriarty's birth - month : "+str(month)+", day : "+str(day)+", year : "+str(year))
+                            print(decd)
+                            plain_text = decd
+
     # END SOLUTION
 
     with open("plain5.txt", "wb") as plain_file:
@@ -152,11 +144,11 @@ def problem_5():
 
 def main():
     try:
-        # problem_1()
-        # problem_2()
-        # problem_3()
+        problem_1()
+        problem_2()
+        problem_3()
         problem_4()
-        # problem_5()
+        problem_5()
     except Exception:
         print("Exception:")
         traceback.print_exc(file=sys.stdout)
